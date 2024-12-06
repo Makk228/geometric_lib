06a85c9b3951a1eb6134f6f9a110a912979d4632
Korsunskiy Maksim <466297@niuitmo.ru>
2024-12-06 14:59:22 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -49,11 +49,13 @@ if __name__ == "__main__":
 
     # Ввод функции
     while func not in funcs:
-        func = input(f"Enter function name, available are {funcs}:\n")
+        func = input(f"Enter function name,\
+        available are {funcs}:\n")
 
     # Ввод размеров в зависимости от выбранной функции и фигуры
     while len(size) != sizes.get(f"{func}-{fig}", 1):
-        size = list(map(int, input("Input figure sizes separated by space").split(' ')))
+        size = list(map(int, input("Input figure\
+        sizes separated by space").split(' ')))
 
     result = calc(fig, func, size)
     print(result)
